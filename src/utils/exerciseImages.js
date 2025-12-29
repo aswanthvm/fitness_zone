a7f71af653e21ff9394d6dcc508884a38abe5c94
Fitness Zone Developer <developer@fitnesszone.local>
2025-12-29 13:33:41 +0530

    Add custom exercise GIF URLs for exercises 0001-0007

--- src/utils/exerciseImages.js
+++ src/utils/exerciseImages.js
@@ -3,6 +3,7 @@ export const localExerciseImages = {
   '0002': 'https://liftmanual.com/wp-content/uploads/2023/04/45-side-bend.gif',
   '0003': 'https://homeworkouts.org/wp-content/uploads/anim-air-bike-crunches.gif',
   '0006': 'https://homeworkouts.org/wp-content/uploads/anim-alternate-heel-touches.gif',
+  '0007': 'https://www.inspireusafoundation.org/wp-content/uploads/2022/03/cable-one-arm-late-pulldown.gif',
 };
 
 export const getExerciseImageUrl = (exercise) => {
